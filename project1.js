1f670d386dbae198a3baff64ab92d297f2e86012
MylesHouse <92052916+MylesHouse@users.noreply.github.com>
2025-11-20 14:46:26 -0500

    Change canvas size and parent attachment
    Updated canvas size to 600x600 and adjusted parent attachment.

--- project1.js
+++ project1.js
@@ -3,10 +3,10 @@ const xh = angle => R / 15.0 * 16 * Math.pow(Math.sin(angle), 3);
 const yh = p => R / 15.0 * (-13 * Math.cos(p) + 5 * Math.cos(2 * p) + 2 * Math.cos(3 * p) + Math.cos(4 * p));
 
 function setup() {
-  createCanvas(400, 400);
+    let cnv = createCanvas(600, 600);  // use your actual size
+  cnv.parent("sketch-container");    // attach canvas to the div
     blendMode(LIGHTEST);
    angleMode(DEGREES);
-  cnv.parent("sketch-container");
 
 }
 
